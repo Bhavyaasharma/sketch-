be3f0fb93098ff295f3f1768fb60abbb26a9c573
Bhavyaasharma <81627599+Bhavyaasharma@users.noreply.github.com>
2022-04-29 18:44:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+function setup()
+{
+    canvas = createCanvas( 280,280);
+    canvas.center();
+    background ("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function preload ()
+{
+    classifier = ml5.imageClassifier();
+}
+
+function draw ()
+{
+    strokeWeight(13);
+    stroke(0);
+    if(mouseIsPresed){
+        line(pmouseX,pmouseY, mouseX,mouseY);
+    }
+}
+
+function classifyCanvas()
+{
+    classifier = classify(canvas,gotResult);
+}
+
+function gotResult(error, results)
+    {
+if(error)
+{
+    console.error (error);
+}
+else{
+    console.log(results);
+    document.getElementById('label').innerHTML = 'Label: ' +results[0].label;
+    document.getElementById('confindence').innerHTML = 'confindence: ' + Math.round (results [0] .confidence*100) + '%';
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+
+}
+
+
+    }
+
+function clearCanvas()
+{
+    background ("white") 
+}
+
